77fb5936d9cd7e156359225b783d938dea93cfa2
Muthash <stephen.kathingo@andela.com>
2018-06-23 13:04:12 +0300

    [Feature] Add Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-import logo from '../static/img/logo.png'
+import '../static/css/navbar.css';
 
 const Navbar = () => (
-    <div>
-        <nav className="navbar transparent navbar-expand-lg navbar-dark fixed-top nav-color">
-          <div className="container">
-            <Link className="navbar-brand" to="/">
-                <img src={logo} width="50" height="40" className="d-inline-block align-top" alt="WeConnect"/>
-            </Link>
-            <button className="navbar-toggler navbar-toggler-right" type="button" 
-                    data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" 
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li className="nav-item active">
-                        <Link className="nav-link" to="/">Home
-                            <span className="sr-only">(current)</span>
-                        </Link>
-                    </li>
-                    <li className="nav-item ">
-                        <Link className="nav-link" to="/">About</Link>
-                    </li>
-                    <li className="nav-item ">
-                        <Link className="nav-link" to="/">About</Link>
-                    </li>
-                </ul>
-                <div className="navbar-nav">
-                    <Link className="nav-item nav-link" to="">Sign in</Link>
-                    <span className="navbar-text">or</span>
-                    <Link className="nav-item nav-link" to="/register">Sign up</Link>
-                </div>
-            </div>
-          </div>
-        </nav>
-    </div>   
+  <div>
+    <nav className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span className="icon icon-bar" />
+            <span className="icon icon-bar" />
+            <span className="icon icon-bar" />
+          </button>
+          <Link to="/" className="navbar-brand">
+            <span>
+              We
+            </span>
+            Connect
+          </Link>
+        </div>
+        <div className="collapse navbar-collapse">
+          <ul className="nav navbar-nav navbar-right">
+            <li className="active">
+              <Link to="/" className="">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/listings" className="smoothScroll">
+                Business Listings
+              </Link>
+            </li>
+            <li>
+              <Link to="/register" className="smoothScroll">
+                Register
+              </Link>
+            </li>
+            <li>
+              <Link to="/login" className="smoothScroll">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>   
 );
 
 export default Navbar;
