2b6df29c9e6c1f8afd56ff6bcaed84f8d6110786
Muthash <stephen.kathingo@andela.com>
2018-06-23 07:56:16 +0300

    [Feature] Add Index body component

---
+++ src/components/IndexBody.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const IndexBody = () => (
+    <div>
+        <section className="masthead text-white d-flex">
+        <div className="container my-auto">
+            <div className="row">
+                <div className="col-lg-7 mx-auto">
+                    <h1>
+                        <strong>Your Favourite Source of All Business Connections </strong>
+                    </h1>
+                    <hr/>
+                    <p className="text-faded mb-5">WeConnect provides a platform that brings businesses and individuals together. This platform creates
+                        awareness for businesses and gives the users the ability to write reviews about the businesses they
+                        have interacted with.
+                    </p>
+                    <hr/>
+                </div>
+            </div>
+        </div>
+        </section>  
+    </div>
+);
+
+export default IndexBody;
