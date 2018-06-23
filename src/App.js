0cfb64967f0b9d45854169c82714bca57b657646
Muthash <stephen.kathingo@andela.com>
2018-06-23 07:49:01 +0300

    [Feature] Add index and register Routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+import IndexPage from './components/IndexPage';
+import RegisterPage from './components/RegisterPage';
+
+const App = () => (
+  <BrowserRouter>
+      <div>
+          <Route exact path="/" component={IndexPage} />
+          <Route exact path="/register" component={RegisterPage} />
       </div>
-    );
-  }
-}
+  </BrowserRouter>
+);
 
 export default App;
