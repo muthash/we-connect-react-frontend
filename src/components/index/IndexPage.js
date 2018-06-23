5c9ea42b02d033891e5c21cbd54dd309e84863c1
Muthash <stephen.kathingo@andela.com>
2018-06-23 13:17:54 +0300

    Add index page component

---
+++ src/components/index/IndexPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Navbar from '../Navbar';
+import IndexBody from './IndexBody';
+import Footer from '../Footer';
+
+const IndexPage = () => (
+  <div>
+    <Navbar />
+    <IndexBody />
+    <Footer />
+  </div>
+);
+
+export default IndexPage;
