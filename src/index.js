66a6c21d0db153561d6d721eae966d8b7470d928
Muthash <stephen.kathingo@andela.com>
2018-06-23 07:52:16 +0300

    [Feature] Import css files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './static/css/bootstrap.css';
+import './static/css/custom.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
