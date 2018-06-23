4fe7f3b10dc78df45efa719c8dd9ed02156af5ca
Muthash <stephen.kathingo@andela.com>
2018-06-23 13:14:05 +0300

    Create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../static/css/footer.css';
+
+const Footer = () => (
+  <div>
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 col-sm-6">
+            <div className="footer-copyright">
+              <p>
+                Copyright &copy;2018 WeConnect
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>   
+);
+
+export default Footer;
