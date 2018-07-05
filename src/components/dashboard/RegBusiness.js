ab3612bfc5d63a4b301aba99484141e817e66e8f
Muthash <stephen.kathingo@andela.com>
2018-07-05 23:51:55 +0300

    [Feature] Add register business component

---
+++ src/components/dashboard/RegBusiness.js
@@ -0,0 +1,85 @@
+import {Component} from 'react';
+
+class RegBusiness extends Component{
+    state = {
+        messageAdd: undefined,
+        messageFail: undefined,
+        nameErr: undefined,
+        descErr: undefined,
+        locationErr: undefined,
+        categoryErr: undefined,
+        disabled: false
+    }
+    
+    handleSubmit = async (event) => {
+      event.preventDefault();
+      
+      const name = event.target.name.value;
+      const desc = event.target.desc.value;
+      const location = event.target.location.value;
+      const category = event.target.category.value;
+      this.setState({disabled: "disabled"});
+
+      const regBusiness = response => {
+        if (response.hasOwnProperty('message') && response.message === "Business created successfully"){
+          this.setState({ messageAdd: response.message });
+        } 
+        if (response.hasOwnProperty('msg')){
+            this.props.history.push({
+                pathname: '/login',
+                state: {
+                    'success': "Please log in to continue",
+                }
+            });
+        }
+        else {
+            this.setState ({
+                messageFail: response.hasOwnProperty('message') && response['message'],
+                nameErr: response.hasOwnProperty('name-error') && response['name-error'],
+                descErr: response.hasOwnProperty('description-error') && response['description-error'],
+                locationErr: response.hasOwnProperty('location-error') && response['location-error'],
+                categoryErr: response.hasOwnProperty('category-error') && response['category-error'],
+                disabled: false
+            });
+        }
+      };
+      await fetch('https://wc-app-api.herokuapp.com/api/v1/businesses', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + this.loggedIn()
+            },
+            body: JSON.stringify(
+                {
+                    "name": name,
+                    "description": desc,
+                    "location": location,
+                    "category": category
+                }
+            )
+        })
+        .then((resp) => resp.json())
+        .then(regBusiness)
+        .catch(function (error) {
+            console.log('Request failed due to', error);
+        });
+    }
+    
+    loggedIn() {
+        // Checks if there is a saved token and it's still valid
+        const token = localStorage.getItem('wcToken');
+        if (token === null) {
+            this.props.history.push({
+                pathname: '/login',
+                state: {
+                  'success': "Please log in to continue",
+                }
+            });
+        } else{
+            return token;
+        }
+    }
+}
+
+export default RegBusiness;
