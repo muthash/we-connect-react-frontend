1c48caf2a573dab861dbd9823759a60c715d62eb
Muthash <stephen.kathingo@andela.com>
2018-07-05 23:42:01 +0300

    [Feature] Add test setup configs

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,5 @@ const Enzyme = require('enzyme');
 const EnzymeAdapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+global.fetch = require('jest-fetch-mock')
