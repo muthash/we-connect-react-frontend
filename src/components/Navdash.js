79d17bf94b66f42c116fdaf56f2373c9155c77f9
Muthash <stephen.kathingo@andela.com>
2018-07-05 23:38:59 +0300

    [Feature] Update to class component

--- src/components/Navdash.js
+++ src/components/Navdash.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-const Navdash = () => (
+const Navdash = ({username, home, listings}) => (
   <div>
     <nav className="navbar navbar-default navbar-fixed-top">
       <div className="container">
@@ -17,21 +17,21 @@ const Navdash = () => (
         </div>
         <div className="collapse navbar-collapse" id="navbar-collapse">
           <ul className="nav navbar-nav">
-            <li className="active">
+            <li className={home}>
               <Link to="/dashboard">Home</Link>
             </li>
-            <li>
+            <li className={listings}>
               <Link to="/businesses">Business listings</Link>
             </li>
           </ul>
           <ul className="nav navbar-nav navbar-right">
             <li className="dropdown">
-              <Link to="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profile
+              <Link to="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                {username ? username : username}
                 <span className="caret" />
               </Link>
               <ul className="dropdown-menu">
                 <li><Link to="/change-password">Change password</Link></li>
-                <li><Link to="#">Settings</Link></li>
                 <li role="separator" className="divider"></li>
                 <li><Link to="/logout">logout</Link></li>
               </ul>
