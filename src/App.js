3a4a318b6660d884df53fafbf96c2db75ef8e00f
Muthash <stephen.kathingo@andela.com>
2018-07-09 14:06:53 +0300

    [Chore] Remove listings route

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import IndexPage from './components/index/IndexPage';
 import RegisterPage from './components/register/RegisterPage';
 import LoginPage from './components/login/LoginPage';
 import Logout from './components/login/Logout';
-import ListingPage from './components/listings/ListingPage';
 import DashPage from './components/dashboard/DashPage';
 import RegistryPage from './components/registry/RegistryPage';
 import BusinessPage from './components/business/BusinessPage';
@@ -32,7 +31,6 @@ const App = () => (
       <Route exact path="/register" component={RegisterPage} />
       <Route exact path="/login" component={LoginPage} />
       <PrivateRoute exact path="/logout" component={Logout} />
-      <PrivateRoute exact path="/listings" component={ListingPage} />
       <PrivateRoute exact path="/dashboard" component={DashPage} />
       <PrivateRoute exact path="/businesses/update/:id" component={UpdateBiz} />
       <PrivateRoute exact path="/businesses" component={RegistryPage} />
